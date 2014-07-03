5daca39f474248e110e1a09e0f156513604f0071
Norman Manantan <normanmanantan@gmail.com>
2014-07-03 10:04:46 +0800

    Change stylesheets dir

--- index.php
+++ index.php
@@ -2,39 +2,55 @@
 <html>
 	<head>
 		<title></title>
-		<link rel="stylesheet" href="">
-	</head>
+		<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/stylesheets/template.css" rel="stylesheet" />
+		<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/stylesheets/bootstrap-theme.min.css" rel="stylesheet" />
+		<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/stylesheets/bootstrap.min.css" rel="stylesheet" />
+		<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/stylesheets/font-awesome.min.css" rel="stylesheet" />
+		</head>
 	<body>
 		<div id="main-wrapper">
-			<div id="header">
-				<jdoc type="modules" position="top-page" />
+			<div id="top-menu-wrapper">
+			<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+			      <div class="container">
+			        <div class="navbar-header">
+			          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			            <span class="sr-only">Toggle navigation</span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			          </button>
+			          <a class="navbar-brand" href="#">Project name</a>
+			        </div>
+			        <div class="navbar-collapse collapse">
+			          	<jdoc:include type="modules" name="top-page" />
+			        </div>
+			      </div>
+			    </div>
 			</div>
 			<div id="middle" class="container"> 
 				<div class="row">
-					<jdoc type="modules" position="heading" />
-	                <jdoc type="modules" position="landing-page" />
-	                <jdoc type="modules" position="featured" />
-	                <jdoc type="modules" position="management" />
-	                <jdoc type="modules" position="about-slider" />
-	                <jdoc type="modules" position="works" />
-	                <jdoc type="modules" position="works-content" />
-	                <jdoc type="modules" position="works-quote" />
-	                <jdoc type="modules" position="process" />
-	                <jdoc type="modules" position="services" />
-	                <jdoc type="modules" position="partners" />
-	                <jdoc type="modules" position="contacts" />
+					<jdoc:include type="component" />
+					<jdoc:include type="modules" name="heading" />
+	                <jdoc:include type="modules" name="landing-page" />
+	                <jdoc:include type="modules" name="featured" />
+	                <jdoc:include type="modules" name="management" />
+	                <jdoc:include type="modules" name="about-slider" />
+	                <jdoc:include type="modules" name="works" />
+	                <jdoc:include type="modules" name="works-content" />
+	                <jdoc:include type="modules" name="works-quote" />
+	                <jdoc:include type="modules" name="process" />
+	                <jdoc:include type="modules" name="services" />
+	                <jdoc:include type="modules" name="partners" />
+	                <jdoc:include type="modules" name="contacts" />
 				</div>
 			</div>
 			<div id="footer">
 				<div class="container-fluid">
 					<div class="row">
 						<div class="clearfix"></div>
-						<div class="col-md-2">
-							<jdoc type="modules" position="bottom-page-left" />
-						</div>
-						<div class="col-md-offset-8 col-md-2">
-							<jdoc type="modules" position="bottom-page-right" />
-						</div>
+						<jdoc:include type="modules" name="bottom-page-left" />
+						<jdoc:include type="modules" name="bottom-page-right" />
+						<jdoc:include type="modules" name="bottom-page-small" />
 					</div>
 				</div>
 			</div>	
